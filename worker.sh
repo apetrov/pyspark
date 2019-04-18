f01f9291c0f3ea9735267a3aed7f3120c1c9641c
Alexander Petrov <alexonrails@gmail.com>
2019-04-18 20:32:13 +0000

    add worker

---
+++ worker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. "/app/spark/sbin/spark-config.sh"
+
+. "/app/spark/bin/load-spark-env.sh"
+
+mkdir -p $SPARK_WORKER_LOG
+
+export SPARK_HOME=/app/spark
+
+ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+
+/app/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
+--webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
